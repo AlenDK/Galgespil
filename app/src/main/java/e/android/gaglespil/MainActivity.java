6c28f33b4618c8820d7985021453e329239c09f6
Alen Hasanagic <Alenhasana@yahoo.dk>
2018-11-09 17:01:20 +0100

    Startskærm Fixed

--- app/src/main/java/e/android/gaglespil/MainActivity.java
+++ app/src/main/java/e/android/gaglespil/MainActivity.java
@@ -1,8 +1,7 @@
 package e.android.gaglespil;
 
-import android.annotation.SuppressLint;
+
 import android.app.Fragment;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -13,7 +12,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
 
 import java.text.DecimalFormat;
 
@@ -21,16 +19,13 @@ public class MainActivity extends Fragment implements View.OnClickListener {
 
     SharedPreferences prefs;
     TextView procentVundet, procentVundet2;
-    int gennemført;
 
-    @SuppressLint("SetTextI18n")
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_main, container, false);
 
 
         procentVundet = (TextView) view.findViewById(R.id.procentVundet);
-        procentVundet2 = (TextView) view.findViewById(R.id.procentVundet2);
 
         Button b1 = (Button) view.findViewById(R.id.hjælp);
         Button b2 = (Button) view.findViewById(R.id.Spil);
@@ -42,19 +37,15 @@ public class MainActivity extends Fragment implements View.OnClickListener {
 
         prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
-
         double antalV = (double) prefs.getInt("RoundV", 0);
         double antal = (double) prefs.getInt("Round", 0);
-
         double procentV = antalV / antal * 100;
 
         DecimalFormat df = new DecimalFormat();
         df.setMaximumFractionDigits(2);
 
-        procentVundet.setText("Du har vundet " + prefs.getInt("RoundV", 0) + " spil ud af " + prefs.getInt("Round", 0));
-
-        procentVundet2.setText("Din winrate er på: " + df.format(procentV));
-
+        procentVundet.setText("Du har vundet " + prefs.getInt("RoundV", 0) + " spil ud af " + prefs.getInt("Round", 0) + " \n"
+                + "Din winrate er på: " + df.format(procentV));
 
         return view;
     }
